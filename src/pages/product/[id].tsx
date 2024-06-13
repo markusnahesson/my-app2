32766157781968e91bdfb80802bf6f1228a3328f
Markus Nahesson <markusnahesson@gmail.com>
2024-06-13 23:07:50 +0700

    Rendering Dynamic Routes, Middleware, Setup Next-Auth

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,14 +1,35 @@
+import { fetcher } from "@/lib/swr/fetcher";
+import DetailProduct from "@/views/DetailProduct";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import { ProductType } from "../types/product.type";
 
-const DetailProductPage = () => {
+const DetailProductPage = ({product}: {product: ProductType}) => {
     const router = useRouter();
+
+    // client-side
+    // const { data, error, isLoading } = useSWR(`/api/product/${router.query.id}`, fetcher);
     
     return (
         <div>
-            <h1>Detail Product</h1>
-            <p>Product : {router.query.id} </p>
+            {/* client-side */}
+            {/* <DetailProduct product={isLoading ? {} : data.data} /> */}
+            {/* server-side */}
+            <DetailProduct product={product} />
         </div>
     )
 };
 
 export default DetailProductPage;
+
+export async function getServerSideProps({ params }: { params: { id:string } }) {
+    // fetch data
+    const res = await fetch(`http://localhost:3000/api/product/${params.id}`)
+    const response = await res.json();
+    
+    return {
+        props: {
+            product: response.data
+        }
+    }
+}
